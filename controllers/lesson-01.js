b126aff1bebb42b741b5e9d38de4ca644f254593
Jadynbug <83982927+Jadynbug@users.noreply.github.com>
2023-10-06 14:45:57 -0500

    W04 06
    this should fix the update problem in the frontend

--- controllers/lesson-01.js
+++ controllers/lesson-01.js
@@ -99,6 +99,11 @@ const deleteContact = async (req, res, next) => {
         console.log(query);
     
         let result = await mongodb.getDb().db('CSE341').collection("contacts").deleteOne(query);
+
+        if(typeof window === "object") {
+            location.reload(true);
+            res.send(result).status(204);
+        };
     
         res.send(result).status(200);
     } catch {
@@ -112,45 +117,50 @@ const updateContact = async (req, res, next) => {
     try {
         if((!req.query['id'] && !req.query['_id']) && (!req.body['id'] && !req.body['_id']) && (!req.params['id'] && !req.params['_id'])) {
             res.send("lack of id error");
-        } 
+        };
         let update = {};
         let id = {};
         if(req.query['id']) {
             id = {"id": req.query['id']};
-        }
+        };
         if(req.query['_id']) {
             id = {"_id": new ObjectId(req.query['_id'])};
-        }
+        };
         if (req.query['id'] || req.query['_id']) {
             if (req.query['firstName']) {update.firstName = req.query['firstName']};
             if (req.query['lastName']) {update.lastName = req.query['lastName']};
             if (req.query['email']) {update.email = req.query['email']};
             if (req.query['favoriteColor']) {update.favoriteColor = req.query['favoriteColor']};
             if (req.query['bithday']) {update.bithday = req.query['bithday']};
-        }
+        };
         if(req.body['id']) {
             id = {"id": req.body['id']};
-        }
+        };
         if(req.body['_id']) {
             id = {"_id": new ObjectId(req.body['_id'])};
-        }
+        };
         if(req.body['id'] || req.body['_id'] || req.params['_id']) {
             if (req.body['firstName']) {update.firstName = req.body['firstName']};
             if (req.body['lastName']) {update.lastName = req.body['lastName']};
             if (req.body['email']) {update.email = req.body['email']};
             if (req.body['favoriteColor']) {update.favoriteColor = req.body['favoriteColor']};
             if (req.body['bithday']) {update.bithday = req.body['bithday']};
-        }
+        };
         if(req.params['id']) {
-            id = {"id": req.body['id']};
-        }
+            id = {"id": req.params['id']};
+        };
         if(req.params['_id']) {
             id = {"_id": new ObjectId(req.params['_id'])};
-        }
+        };
         // console.log(update);
         let newValues = {$set: update};
 
         let result = await mongodb.getDb().db('CSE341').collection("contacts").updateOne(id, newValues);
+
+        if(typeof window === "object") {
+            location.reload(true);
+            res.send(result).status(204);
+        };
 
         res.send(result).status(204);
     } catch {
